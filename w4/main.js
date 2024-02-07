d6cb67725113eb697194c203a039923d5c0b06c2
Deonte Hampton <deonte2313@icloud.com>
2024-02-07 04:12:50 -0800

    Setup Done

---
+++ w4/main.js
@@ -0,0 +1,89 @@
+console.log("Top of file");
+const cfpData = [];
+function determineSizePts(houseSize) {
+  console.log("inside block scope");
+  let houseStyle = 0;
+  if (houseSize === "large") {
+    houseStyle = 10;
+  } else if (houseSize === "medium") {
+    houseStyle = 7;
+  } else if (houseSize === "small") {
+    houseStyle = 4;
+  } else if (houseSize === "apartment") {
+    houseStyle = 2;
+  }
+  
+  return houseStyle;
+}
+function determineHouseHoldPts(numberInHousehold) {
+  console.log("inside block scope");
+  let houseHoldPoints = 0;
+  if (numberInHousehold === 1) {
+    houseHoldPoints = 14;
+  } else if (numberInHousehold === 2) {
+    houseHoldPoints = 12;
+  } else if (numberInHousehold === 3) {
+    houseHoldPoints = 10;
+  } else if (numberInHousehold === 4) {
+    houseHoldPoints = 8;
+  } else if (numberInHousehold === 5) {
+    houseHoldPoints = 6;
+  } else if (numberInHousehold === 6) {
+    houseHoldPoints = 4;
+  } else if (numberInHousehold > 6) {
+    houseHoldPoints = 2;
+  }
+
+  return houseHoldPoints;
+}
+
+
+console.log("global scope");
+
+
+function start(houseHoldMembers, houseArea) {
+  const houseHoldPTS = determineHouseHoldPts(houseHoldMembers);
+  const houseSchemantics = determineSizePts(houseArea);
+  const total = houseHoldPTS + houseSchemantics;
+  cfpData.push([
+    houseHoldMembers,
+    houseArea,
+    houseHoldPTS,
+    houseSchemantics,
+    total]
+  );
+ 
+ 
+}
+
+function displayOutput(){
+  
+}
+
+start(1,"small");
+start(1,"medium");
+start(1,"large");
+start(1,"apartment");
+start(2,"small");
+start(2,"medium");
+start(2,"large");
+start(2,"apartment");
+start(3,"small");
+start(3,"medium");
+start(3,"large");
+start(3,"apartment");
+start(4,"small");
+start(4,"medium");
+start(4,"large");
+start(4,"apartment");
+start(5,"small");
+start(5,"medium");
+start(5,"large");
+start(5,"apartment");
+start(6,"small");
+start(6,"medium");
+start(6,"large");
+start(6,"apartment");
+
+
+displayOutput ()
